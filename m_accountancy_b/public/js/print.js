4666806d3a7760e3689f566a7d37e772dafec380
alidzhikov <m.alidzhikov2630@gmail.com>
2016-11-01 14:52:56 +0200

    unnecessary files

--- m_accountancy_b/public/js/print.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Laptop on 3.4.2016 г..
- */
-//$( document ).ready(function() {
-//    window.print();
-//});
-setTimeout( function(){window.print();}, 100);
--- m_accountancy_b/resources/views/errors/list.blade.php
+++
@@ -1,7 +0,0 @@
-@if($errors->any())
-    <ul class="alert alert-danger">
-        @foreach($errors as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
